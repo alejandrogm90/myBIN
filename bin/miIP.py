c6904889d5455032357eb526df780f251371a37b
alejandrogm90 <alejandrogomezmartin90@gmail.com>
2021-06-04 10:12:21 +0200

    * Update miIP.py

--- bin/miIP.py
+++ bin/miIP.py
@@ -21,6 +21,7 @@ import sys
 import socket
 import urllib.request
 
+
 def IP_local():
     respuesta = ""
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,6 +29,7 @@ def IP_local():
     respuesta = s.getsockname()[0]
     s.close()
     return respuesta
+
 
 def IP_global():
     response = urllib.request.urlopen('http://www.vermiip.es/')
@@ -38,11 +40,12 @@ def IP_global():
     cad1 = cad1.split(' ')[4].split('<')[0]
     return cad1
 
+
 if __name__ == '__main__':
-    if len(sys.argv) >=2:
-        if sys.argv[1] == "-l" or sys.argv[1] == "--local" :
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == "-l" or sys.argv[1] == "--local":
             print(IP_local())
-        elif sys.argv[1] == "-g" or sys.argv[1] == "--global" :
+        elif sys.argv[1] == "-g" or sys.argv[1] == "--global":
             print(IP_global())
         else:
             print("ERROR:")
@@ -50,6 +53,5 @@ if __name__ == '__main__':
             print(sys.argv[0]+" -g or --global #GLOBAL IP")
             print(sys.argv[0]+" #GLOBAL IP")
     else:
-        print(IP_global())
-
-    
+        print('Local_IP:'+IP_local())
+        print('Global_IP:'+IP_global())
